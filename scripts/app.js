a6a98baf1026b0a98c762c55c6f1cf467da50a03
Matthew Caldwell <mcaldwell@mediamarketers.com>
2016-04-11 16:48:47 -0400

    another session

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,12 @@ angular.module('todolistapp', []).controller('mainCtrl', function($scope) {
     $scope.helloWorld = function() {
         console.log("Hello there!");
     };
+    $scope.todos = [
+        {'name': 'clean the house'},
+        {'name': 'water the dog'},
+        {'name': 'feed the lawn'},
+        {'name': 'pay dem bills'},
+        {'name': 'run'},
+        {'name': 'swim'}
+    ];
 });
